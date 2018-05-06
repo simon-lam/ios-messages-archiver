5e41e5e456681ec560a59092b05de60fb3d5aa7d
Simon Lam <simonlam6@gmail.com>
2018-05-06 13:47:39 -0500

    Inject path to Google client secret file instead of hardwire

--- app/modules/Module.scala
+++ app/modules/Module.scala
@@ -5,16 +5,18 @@ import java.io.FileReader
 import com.google.api.client.googleapis.auth.oauth2.{GoogleAuthorizationCodeFlow, GoogleClientSecrets}
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.client.util.store.{FileDataStoreFactory, MemoryDataStoreFactory}
+import com.google.api.client.util.store.MemoryDataStoreFactory
 import com.google.api.services.gmail.GmailScopes
 import com.google.inject.AbstractModule
+import com.typesafe.config.Config
 
 import scala.collection.JavaConverters._
-class Module extends AbstractModule {
+
+class Module(config: Config) extends AbstractModule {
 
   override def configure(): Unit = {
     bind(classOf[GoogleAuthorizationCodeFlow]).toInstance({
-      val clientSecretFile = "/Users/simonlam/Desktop/client_secret_368401229586-vrsfbrfsr8k4u4ac5mu56h6d0a8l9hlp.apps.googleusercontent.com.json"
+      val clientSecretFile = config.getString("google.clientSecretFile")
       val clientSecrets = GoogleClientSecrets.load(JacksonFactory.getDefaultInstance(), new FileReader(clientSecretFile))
 
       new GoogleAuthorizationCodeFlow.Builder(new NetHttpTransport(),
